bf598cf3a1113768d80e7bb4e0a045f166c6cd19
isurucis <143800833+isurucis@users.noreply.github.com>
2024-10-31 18:12:33 +0530

    Update settings.py
    db setting updated

--- LiveVMS/LiveVMS/settings.py
+++ LiveVMS/LiveVMS/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'LiveVMS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'aggregatedvendor$default',
+        'USER': 'aggregatedvendor',
+        'PASSWORD': 'Isuru@1984',
+        'HOST': 'aggregatedvendor.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
 
