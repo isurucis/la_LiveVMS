4cb7ab44475b606cf41e7016e2433161cda6eca2
isurucis <143800833+isurucis@users.noreply.github.com>
2024-10-31 21:38:48 +0530

    Update admin.py
    test

--- LiveVMS/vendors/admin.py
+++ LiveVMS/vendors/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from .models import Vendor
+from products.models import Product
 
+class ProductInline(admin.TabularInline):
+    model = Product
+    extra = 1
 @admin.register(Vendor)
 class VendorAdmin(admin.ModelAdmin):
     list_display = ('name', 'address', 'is_active')
     list_filter = ('is_active',)
     search_fields = ('name', 'address')
+    
+    inlines = [ProductInline] 
