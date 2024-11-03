7418b82944822310f857311d79033a148e6b7ae7
isurucis <143800833+isurucis@users.noreply.github.com>
2024-11-03 19:56:34 +0530

    test

--- LiveVMS/LiveVMS/settings.py
+++ LiveVMS/LiveVMS/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'import_export',
     'vendors',
     'products',
     'orders',
     'campaigns',
+    
 ]
 
 MIDDLEWARE = [
--- LiveVMS/products/admin.py
+++ LiveVMS/products/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import ProductCategory, Product
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 @admin.register(ProductCategory)
 class ProductCategoryAdmin(admin.ModelAdmin):
@@ -7,8 +9,14 @@ class ProductCategoryAdmin(admin.ModelAdmin):
     list_filter = ('parent',)
     search_fields = ('name',)
 
+class ProductResource(resources.ModelResource):
+    class Meta:
+        model = Product
+        fields = ('name', 'scientific_name', 'category', 'vendor', 'vendor_code', 'cis_code', 'laq_code', 'stock', 'price', 'is_active',)  # Specify the fields to import/export
+        
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(ImportExportModelAdmin):
+    resource_class = ProductResource
     list_display = ('name', 'scientific_name', 'category', 'vendor', 'stock', 'price', 'is_active', 'created_date')
     list_filter = ('category', 'vendor', 'is_active')
     search_fields = ('name', 'scientific_name', 'vendor_code', 'cis_code', 'laq_code')
