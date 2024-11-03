5e2e76a95ad105bef9974494a6f06daa087e0024
isurucis <143800833+isurucis@users.noreply.github.com>
2024-11-03 23:17:54 +0530

    test

--- LiveVMS/campaigns/admin.py
+++ LiveVMS/campaigns/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from .models import  Campaign, CampaignProduct, CampaignChannel
+from .forms import CampaignProductForm
 
 class CampaignProductInline(admin.TabularInline):
     model = CampaignProduct
+    form = CampaignProductForm
     extra = 1
+
+    def get_formset(self, request, obj=None, **kwargs):
+        formset = super().get_formset(request, obj, **kwargs)
+        # If the campaign has a category, pass it to the form
+        if obj and obj.category:
+            for form in formset.forms:
+                form.fields['product'].queryset = form.fields['product'].queryset.filter(category=obj.category)
+        return formset
+
 
 class CampaignChannelInline(admin.TabularInline):
     model = CampaignChannel
