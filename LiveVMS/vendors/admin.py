2bd917b39be5904551dda7a146bb99ae51e1dee4
isurucis <143800833+isurucis@users.noreply.github.com>
2024-11-08 15:47:18 +0530

    update vender model

--- LiveVMS/vendors/admin.py
+++ LiveVMS/vendors/admin.py
@@ -1,18 +1,26 @@
 from django.contrib import admin
+from import_export import resources
 from .models import Vendor
 from products.models import Product
+from import_export.admin import ImportExportModelAdmin
 
 class ProductInline(admin.TabularInline):
     model = Product
     extra = 1
+class VendorResource(resources.ModelResource):
+    class Meta:
+        model = Vendor
+        fields = ('name', 'etf_vendor_code', 'vendor_id', 'country', 'status')
+
 @admin.register(Vendor)
-class VendorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'address', 'is_active')
-    list_filter = ('is_active',)
-    search_fields = ('name', 'address')
-    
-    inlines = [ProductInline] 
+class VendorAdmin(ImportExportModelAdmin):
+    resource_class = VendorResource
+    list_display = ('name', 'etf_vendor_code', 'vendor_id', 'country', 'status')
+    list_filter = ('country', 'status')
+    search_fields = ('name', 'etf_vendor_code', 'vendor_id')
+    inlines = [ProductInline]
 
 admin.site.site_header = "LiveVMS Admin"
 admin.site.site_title = "Live VMS"
 admin.site.index_title = "Live VMS"
+
--- LiveVMS/vendors/models.py
+++ LiveVMS/vendors/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
 class Vendor(models.Model):
-    name = models.CharField(max_length=255)
-    address = models.TextField()
-    is_active = models.BooleanField(default=True)
+    STATUS_CHOICES = [
+        ('active', 'Active'),
+        ('deactivated', 'Deactivated'),
+        ('inactive', 'Temporarily Inactive'),
+        ('pending', 'Pending'),
+    ]
+
+    name = models.CharField(max_length=255, verbose_name="Vendor Name")
+    etf_vendor_code = models.CharField(max_length=10, verbose_name="ETF Vendor Code")
+    vendor_id = models.CharField(max_length=10, unique=True, verbose_name="Vendor ID")
+    country = models.CharField(max_length=100)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, verbose_name="Vendor Status")
 
     def __str__(self):
         return self.name
