12be1bc7aa73e7389a338f50840a1d6f52d79c78
isurucis <143800833+isurucis@users.noreply.github.com>
2024-11-08 16:05:59 +0530

    Update models.py
    update vender model

--- LiveVMS/vendors/models.py
+++ LiveVMS/vendors/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Vendor(models.Model):
     STATUS_CHOICES = [
         ('active', 'Active'),
@@ -9,9 +10,9 @@ class Vendor(models.Model):
     ]
 
     name = models.CharField(max_length=255, verbose_name="Vendor Name")
-    etf_vendor_code = models.CharField(max_length=10, verbose_name="ETF Vendor Code")
-    vendor_id = models.CharField(max_length=10, unique=True, verbose_name="Vendor ID")
-    country = models.CharField(max_length=100)
+    etf_vendor_code = models.CharField(max_length=10, verbose_name="ETF Vendor Code", null=True, blank=True)
+    vendor_id = models.CharField(max_length=10, unique=True, verbose_name="Vendor ID", null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, verbose_name="Vendor Status")
 
     def __str__(self):
