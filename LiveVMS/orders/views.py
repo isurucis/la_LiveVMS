1ec0ff5198d93e11a9af3122009c6ff7cd835413
isurucis <143800833+isurucis@users.noreply.github.com>
2024-11-19 16:09:44 +0530

    add shimpment calender

--- LiveVMS/orders/views.py
+++ LiveVMS/orders/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic import DetailView
+from .models import Shipment
+class ShipmentCalendarView(TemplateView):
+    template_name = 'shipment.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Fetch all campaigns to display on the calendar
+        context['shipments'] = Shipment.objects.all()
+        return context
 
-# Create your views here.
+class ShipmentDetailView(DetailView):
+    model = Shipment
+    template_name = 'detail.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Include the products associated with this campaign
+        context['products'] = self.object.campaign_products.select_related('product')
+        return context
