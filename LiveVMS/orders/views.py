b878d3a1b9cc0442fa6ccbdb1a120e2f280c6113
isurucis <143800833+isurucis@users.noreply.github.com>
2024-11-19 16:28:16 +0530

    test

--- LiveVMS/orders/views.py
+++ LiveVMS/orders/views.py
@@ -18,5 +18,6 @@ class ShipmentDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         # Include the products associated with this campaign
-        context['products'] = self.object.campaign_products.select_related('product')
+        context['products'] = self.object.products.select_related('shipment')
+        
         return context
