94d2b4558daa3340f3e19ddb31848e7f2c00b35b
isurucis <143800833+isurucis@users.noreply.github.com>
2024-11-19 16:20:56 +0530

    bugfix

--- LiveVMS/orders/urls.py
+++ LiveVMS/orders/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ShipmentCalendarView, ShipmentDetailView
 
 urlpatterns = [
-    path('', ShipmentCalendarView.as_view(), name='campaign_calendar'),
-    path('<int:pk>/', ShipmentDetailView.as_view(), name='campaign_detail'),
+    path('', ShipmentCalendarView.as_view(), name='shipment_calendar'),
+    path('<int:pk>/', ShipmentDetailView.as_view(), name='shipment_detail'),
 ]
