61b44d1010c18c32fa3135c0af1d4e37fc342a78
isurucis <143800833+isurucis@users.noreply.github.com>
2024-11-19 12:29:42 +0530

    make verder id primery key

--- LiveVMS/vendors/models.py
+++ LiveVMS/vendors/models.py
@@ -2,16 +2,17 @@ from django.db import models
 
 
 class Vendor(models.Model):
+    
     STATUS_CHOICES = [
         ('active', 'Active'),
         ('deactivated', 'Deactivated'),
         ('inactive', 'Temporarily Inactive'),
         ('pending', 'Pending'),
     ]
-
+    
+    vendor_id = models.CharField(max_length=10, verbose_name="Vendor ID", primary_key=True)  # Make this the primary key
     name = models.CharField(max_length=255, verbose_name="Vendor Name")
     etf_vendor_code = models.CharField(max_length=10, verbose_name="ETF Vendor Code", null=True, blank=True)
-    vendor_id = models.CharField(max_length=10, unique=True, verbose_name="Vendor ID", null=True, blank=True)
     country = models.CharField(max_length=100, null=True, blank=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, verbose_name="Vendor Status")
 
