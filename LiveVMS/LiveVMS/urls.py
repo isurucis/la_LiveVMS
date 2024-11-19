f95623259bf44450d2068c2b5cda3714a9f94776
isurucis <143800833+isurucis@users.noreply.github.com>
2024-11-19 16:16:53 +0530

    test

--- LiveVMS/LiveVMS/urls.py
+++ LiveVMS/LiveVMS/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('campaigns/', include('campaigns.urls')),
+    path('shipment/', include('orders.urls')),
 ]
