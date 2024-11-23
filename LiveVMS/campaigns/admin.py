3d92ed577a5097cf2909ef56d1ca5444dc0f498d
isurucis <143800833+isurucis@users.noreply.github.com>
2024-11-23 13:58:06 +0530

    Update admin.py
    update campaign product with easy product selection feature

--- LiveVMS/campaigns/admin.py
+++ LiveVMS/campaigns/admin.py
@@ -1,16 +1,21 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 from .models import  Campaign, CampaignProduct, CampaignChannel
+from unfold.admin import StackedInline, TabularInline
 
-class CampaignProductInline(admin.TabularInline):
+class CampaignProductInline(TabularInline):
     model = CampaignProduct
     extra = 1
+    tab = True
+    raw_id_fields= ["product",]
 
-class CampaignChannelInline(admin.TabularInline):
+class CampaignChannelInline(TabularInline):
     model = CampaignChannel
     extra = 1
+    tab = True
 
 @admin.register(Campaign)
-class CampaignAdmin(admin.ModelAdmin):
+class CampaignAdmin(ModelAdmin):
     list_display = ('name', 'campaign_type', 'start_date', 'end_date')
     inlines = [CampaignProductInline, CampaignChannelInline]
     search_fields = ('name', 'campaign_type')
