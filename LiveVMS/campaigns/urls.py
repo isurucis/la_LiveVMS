009a72110ff7551d8348fda9a7c0c2b8a1f19fac
isurucis <143800833+isurucis@users.noreply.github.com>
2024-11-26 12:01:37 +0530

    update shedule calender

--- LiveVMS/campaigns/urls.py
+++ LiveVMS/campaigns/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import CampaignCalendarView, CampaignDetailView
+from .views import CampaignCalendarView, CampaignDetailView, CampaignPromoDetailView
 
 urlpatterns = [
     path('', CampaignCalendarView.as_view(), name='campaign_calendar'),
+    path('schedule/<int:pk>/', CampaignPromoDetailView.as_view(), name='schedule-detail'),
     path('<int:pk>/', CampaignDetailView.as_view(), name='campaign_detail'),
+    
 ]
+
--- LiveVMS/campaigns/views.py
+++ LiveVMS/campaigns/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic import DetailView
-from .models import Campaign
+from .models import Campaign, CampaignPromo
 class CampaignCalendarView(TemplateView):
     template_name = 'campaigns/calendar.html'
     
@@ -20,3 +20,19 @@ class CampaignDetailView(DetailView):
         # Include the products associated with this campaign
         context['products'] = self.object.campaign_products.select_related('product')
         return context
+
+# class ScheduleDetailView(DetailView):
+#     model = CampaignPromo
+#     template_name = 'campaigns/schedule.html'
+    
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         # Include the products associated with this campaign
+#         context['products'] = self.object.campaign_promo.select_related('product')
+#         return context
+    
+
+class CampaignPromoDetailView(DetailView):
+    model = CampaignPromo
+    template_name = "campaigns/schedule_detail.html"  # Customize the template path if needed
+    context_object_name = "campaignpromo"       # Access the object in the template with this name
